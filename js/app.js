1269cb024693abbc85208a61cdddea421279286c
Vinay Varshney <vinay.varshney28@gmail.com>
2018-04-07 04:29:59 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,98 +1,116 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-$(".indicator").show();
-$(".firstHide").hide();
-
-function getResponse(){
-
-var fbToken= $("input").val();
-
-	
-	$.ajax('https://graph.facebook.com/me?fields=hometown,email,about,birthday,education,name,location,work&access_token='+fbToken,{
-
-success : function(response){
-
-$(".hideArea").hide();
-$(".firstHide").show();
-console.log(response);
-console.log(typeof(response));
-$('#image').attr("src", "https://graph.facebook.com/"+response.id+"/picture?type=normal&height=400&width=400");
-$("#name").text("Name: "+response.name);
-$("#hometown").text("Hometown: "+response.hometown.name);
-$("#email").text("Email: "+response.email);
-$("#about").text("About: "+response.about);
-$("#birthday").text("Birthday: "+response.birthday);
-$("#education").text("Education: "+response.education[0].school.name);
-$("#location").text("Location: "+response.location.name);
-$("#work").text("Work: "+response.work[0].employer.name);
+    $(".firstHide").hide();
 
 
+    function getResponse() {
 
-},//end of success
-
-error:function(request,errorType,errorMessage){
-
-			console.log(request);
-			console.log(errorType);
-			console.log(errorMessage);
-			alert("Token expired! Please enter a valid token");
-		},
-
-		timeout:3000,
-		beforeSend:function(){
-			$(".indicator").show();
-		},
-
-		complete:function(){
-			$(".indicator").hide();
-		}
+            var fbToken = $("input").val();
 
 
-	});//1st ajax end
+            $.ajax('https://graph.facebook.com/me?fields=about,hometown,id,name,first_name,last_name,birthday,languages,gender,education,work,relationship_status,quotes,family,website,email,picture.width(600).height(600),cover&access_token=' + fbToken, {
 
-$.ajax('https://graph.facebook.com/me/posts?limit=16&access_token='+fbToken,{
+                success: function(response) {
 
-success : function(response){
-	$.each(response.data,function(i,val){
+                    $(".hideArea").hide();
+                    $(".jumbotron").hide();
+                    $(".profile-details").show();
 
-		var j=i+1;
-	console.log(j);
-	if(val.message)
-	{
-		$("#posts").append('<div class="container"><div class="row"><h4>Post'+j+'</h4><h6 class="text-muted">Created Time : '+val.created_time+'</h6><p>Message : '+val.message+'</p><a href="#" class="card-link">Post ID : '+val.id+'</a></div><hr></div>');
-	}//end of if
+                    //to switch over profile details and Feeds.
 
-	else {
-			$("#posts").append('<div class="container"><div class="row"><h4>Post'+j+'</h4><h6 class="text-muted">Created Time : '+val.created_time+'</h6><p>Story : '+val.story+'</p><a href="#" class="card-link">Post ID : '+val.id+'</a></div><hr></div>');
+                    $(".btn-success").click(function() {
+                        $(".profile-details").fadeOut(1000);
+                        $(".recent-feed").fadeIn(2000);
+                    });
 
-	} //end of else
+                    console.log(response);
+                    console.log(typeof(response));
+                    $('#cover').attr("src", "" + response.cover.source + "");
+                    $('#profile').attr("src", "" + response.picture.data.url + "");
 
-	});//end of each
-}, //end of success
+                    $("#name").text("Name: " + response.name);
+                    $("#hometown").text("Hometown: " + response.hometown.name);
+                    $("#email").text("Email: " + response.email);
+                    $("#birthday").text("Birthday: " + response.birthday);
 
-		error:function(request,errorType,errorMessage){
+                }, //end of success
 
-			console.log(request);
-			console.log(errorType);
-			console.log(errorMessage);
-			alert("Token expired! Please enter a valid token");
-		},
+                error: function(request, errorType, errorMessage) {
 
-		timeout:3000,
-		beforeSend:function(){
-			$(".indicator").show();
-		},
+                    console.log(request);
+                    console.log(errorType);
+                    console.log(errorMessage);
+                    alert("Token expired! Please enter a valid token");
+                },
 
-		complete:function(){
-			$(".indicator").hide();
-		}
+                timeout: 3000,
+                beforeSend: function() {
+                    $(".indicator").show();
+                },
 
-	});//end of 2nd ajax
-
-}//end of getResponse
-
-	$("button").on("click",getResponse);
+                complete: function() {
+                    $(".indicator").hide();
+                }
 
 
-});//document ready end
+            }); //1st ajax end
 
+            $.ajax('https://graph.facebook.com/me/posts?limit=16&access_token=' + fbToken, {
+
+                success: function(response) {
+                    $.each(response.data, function(i, val) {
+
+                        var j = i + 1;
+                        console.log(j);
+                        console.log(val);
+                        if (val.message) {
+                            $("#posts").append(`
+<h4>Post : ${j}</h4>
+<h6 class="text-muted">Created Time :${val.created_time}</h6>
+<p>Message : ${val.message} </p>
+<p><a href="#" class="card-link">Post ID :${val.id}</a></p>
+<br>
+<hr>
+`);
+
+                        } //end of if
+                        else {
+                            $("#posts").append(`
+<h4>Post : ${j}</h4>
+<h6 class="text-muted">Created Time :${val.created_time}</h6>
+<p>Message : ${val.story} </p>
+<p><a href="#" class="card-link">Post ID :${val.id}</a></p>
+<br>
+<hr>
+
+`);
+                        } //end of else
+
+                    }); //end of each
+                }, //end of success
+
+                error: function(request, errorType, errorMessage) {
+
+                    console.log(request);
+                    console.log(errorType);
+                    console.log(errorMessage);
+                    alert("Token expired! Please enter a valid token");
+                },
+
+                timeout: 3000,
+                beforeSend: function() {
+                    $(".indicator").show();
+                },
+
+                complete: function() {
+                    $(".indicator").hide();
+                }
+
+            }); //end of 2nd ajax
+
+        } //end of getResponse
+
+    $(".submit").on("click", getResponse);
+
+
+}); //document ready end
